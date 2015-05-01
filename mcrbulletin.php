277e18e4e856bd1cf514865be41a31712fa15da3
Richard Gunning <rjgunning@gmail.com>
2015-05-01 16:10:41 +0100

    Update mcrbulletin.php

--- mcrbulletin.php
+++ mcrbulletin.php
@@ -56,9 +56,40 @@ function bulletin_plugin_options() {
 }
 
 function email_members($post_ID)  {
-    global $wpdb;
-    
-    mail("rjgunning@gmail.com", "New WordPress recipe online!", 'A new recipe have been published on http://www.wprecipes.com');
+        global $wpdb;
+         // subject
+        $subject = 'Birthday Reminders for August';
+
+        // message
+        $message = '
+        <html>
+        <head>
+          <title>Birthday Reminders for August</title>
+        </head>
+        <body>
+          <p>Here are the birthdays upcoming in August!</p>
+          <table>
+            <tr>
+              <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+            </tr>
+            <tr>
+              <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+            </tr>
+            <tr>
+              <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+            </tr>
+          </table>
+        </body>
+        </html>
+        ';
+
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+        // Additional headers
+        $headers .= 'From: Clare MCR secretary <mcr-secretary@clare.cam.ac.uk>' . "\r\n";
+        
+    mail("rjgunning@gmail.com", $Subject, $message, $headers);
     return $post_ID;
 }
 
