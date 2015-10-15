21cc28fe0fbe32939dc2763106c2dd2493fc5b5a
RGunning <rjgunning@gmail.com>
2015-10-15 17:29:12 +0100

    added disclaimer and reworked default message

--- mcrbulletin.php
+++ mcrbulletin.php
@@ -98,11 +98,16 @@ function bulletin_plugin_options() {
 		<tr><td>To:</td><td><input type="text" name="to" value="clare-mcr@lists.cam.ac.uk" style="width: 300px;" /></td></tr>
 		<tr><td>From:</td><td><input type="text" name="from" value="mcr-secretary@clare.cam.ac.uk" style="width: 300px;" /></td></tr>
 		<tr><td>Message:</td><td><textarea name="header" form="usrform" style="width: 300px;height:300px">
-<p>Hi everyone,</p>
-<p>Here is the Clare MCR Weekly Bulletin.</p>
+<p>Dear Clare MCR,</p>
+<p>The Weekly Clare MCR Bulletin has been included below.</p>
 <p>If you want to have something included in the next bulletin drop me an <a href="mailto:mcr-secretary@clare.cam.ac.uk">email</a>.</p>
-<p>A new MCR Bulletin newsletters will be sent out every Thursday with the latest events. View the <a href="http://mcr.clare.cam.ac.uk/category/mcr-bulletin">website</a> to see the full list of bulletin items.</p>
-Richard</textarea></td></tr>
+<p>A new MCR Bulletin newsletter will be sent out every Thursday with the latest events. View the <a href="http://mcr.clare.cam.ac.uk/category/mcr-bulletin">website</a> to see the full list of bulletin items.</p>
+
+Kind Regards,
+Richard Gunning
+Clare MCR Secretary
+
+<hr><p style="font-size: smaller;">Clare College MCR hold no responsibility for the content of Bulletin items, see <a href="http://mcr.clare.cam.ac.uk/disclaimer">disclaimer</a>. The MCR bulletin is an unmoderated news feed of adverts sent to us to display. The Clare College MCR does not control, monitor or guarantee the information contained in the MCR bulletin or information contained in links to other external websites, and does not endorse any views expressed or products or services offered therein. All items on the MCR bulletin are viewable via the MCR website, social media and email as part of an effort to engage the broader Cambridge community. If you take offence with any item, please contact us by <a href="mailto:mcr-secretary@clare.cam.ac.uk">email</a> and we will endeavour to remove or edit the item to remove offence after appropriate contact the relevant advertising parties.</p><hr></textarea></td></tr>
 		<tr><td><input type="submit" name="submit" value="Send Email"></td></tr></table>
 	</form>
 <?php }
