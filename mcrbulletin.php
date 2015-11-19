3d700db84eb463125e87385f408fc668a5581d12
RGunning <rjgunning@gmail.com>
2015-11-19 12:23:07 +0000

    fix text encoding

--- mcrbulletin.php
+++ mcrbulletin.php
@@ -103,9 +103,9 @@ function bulletin_plugin_options() {
 <p>If you want to have something included in the next bulletin drop me an <a href="mailto:mcr-secretary@clare.cam.ac.uk">email</a>.</p>
 <p>A new MCR Bulletin newsletter will be sent out every Thursday with the latest events. View the <a href="http://mcr.clare.cam.ac.uk/category/mcr-bulletin">website</a> to see the full list of bulletin items.</p>
 
-Kind Regards,
-Richard Gunning
-Clare MCR Secretary
+<p>Kind Regards,<br>
+Richard Gunning<br>
+Clare MCR Secretary</p>
 
 <hr><p style="font-size: smaller;">Clare College MCR hold no responsibility for the content of Bulletin items, see <a href="http://mcr.clare.cam.ac.uk/disclaimer">disclaimer</a>. The MCR bulletin is an unmoderated news feed of adverts sent to us to display. The Clare College MCR does not control, monitor or guarantee the information contained in the MCR bulletin or information contained in links to other external websites, and does not endorse any views expressed or products or services offered therein. All items on the MCR bulletin are viewable via the MCR website, social media and email as part of an effort to engage the broader Cambridge community. If you take offence with any item, please contact us by <a href="mailto:mcr-secretary@clare.cam.ac.uk">email</a> and we will endeavour to remove or edit the item to remove offence after appropriate contact the relevant advertising parties.</p><hr></textarea></td></tr>
 		<tr><td><input type="submit" name="submit" value="Send Email"></td></tr></table>
@@ -120,11 +120,11 @@ function email_members($message, $to, $from)  {
         $subject = 'MCR Bulletin ' .current_time('d-m-Y');
 
         // message
-        $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers  = "MIME-Version: 1.0" . "\r\n"; 
+        $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n"; 
 
         // Additional headers
-        $headers .= 'From: Clare MCR secretary <'. $from. '>' . "\r\n";
+        $headers .= "From: Clare MCR secretary <". $from. ">\r\n";
 
     mail($to, $subject, $message, $headers);
     return TRUE;
